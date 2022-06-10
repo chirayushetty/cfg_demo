43abc2a0319fb6bd4a1cb661ba81b0c2d6af52f3
fatmab2809 <fatmab2809@gmail.com>
2022-06-10 17:50:56 +0530

    Files added

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const style={
+    backgroundColor: "#F8F8F8",
+    padding: "20px",
+    textAlign: "center",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "60px",
+    width: "100%"
+
+}
+export default function Footer() {
+  return (
+    <div>
+      <div style={style}>Footer</div>
+
+    </div>
+  )
+}
